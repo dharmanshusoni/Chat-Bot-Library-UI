a636b40da5b032c13fb9468dbf6b2c1c9c3da68b
dharmanshu soni <dharmanshu4@gmail.com>
2023-12-07 00:34:41 +0530

    chat options integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-bot-library';
-  chatMessages: { content: string; isUser: boolean }[] = [
-    { content: "Hi there! How can I assist you?", isUser: false },
+  chatMessages: { class?:string, content: string; isUser: boolean; isButton?: boolean; buttonLabel?: string; buttonAction?: string }[] = [
+    { class:'message bot-message',content: "Hi there! How can I assist you?", isUser: false },
+    {
+      content: "We provide a range of services. Would you like more information?",
+      isUser: true,
+      isButton: true,
+      buttonLabel: "Yes, tell me more",
+      buttonAction: "moreInfo",
+      class:'message user-message-option',
+    }
   ];
 
   userInput: string = '';
 
-  toggleChat() {
+  async toggleChat() {
     const chatPopup = document.getElementById('chat-popup');
     if (chatPopup) {
       chatPopup.style.display = (chatPopup.style.display === 'none' || chatPopup.style.display === '') ? 'block' : 'none';
     }
   }
 
-  sendMessage() {
+  async sendMessage() {
     if (this.userInput.trim() !== "") {
-      this.chatMessages.push({ content: this.userInput, isUser: true });
-      this.chatMessages.push({ content: 'Processing: ' + this.userInput, isUser: false });
+      this.chatMessages.push({ class:'message user-message',content: this.userInput, isUser: true });
+      this.chatMessages.push({ class:'message bot-message', content: 'Processing: ' + this.userInput, isUser: false });
       this.userInput = '';
     }
+  }
+
+  async handleButtonClick(action: any) {
+    // Handle button click based on the specified action
+    if (action === 'moreInfo') {
+      this.chatMessages.push({ class:'message bot-message',content: 'Here is more information about our services...', isUser: false });
+    }
+    // Add more button actions as needed
   }
 }
